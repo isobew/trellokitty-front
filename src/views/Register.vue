782aa1d04783dca105d208eccf01efb19c3d575d
Isabella Firmino <isabella.firmino@watchlabs.media>
2025-04-04 19:54:39 -0300

    creating register styles

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="container mx-auto">
-    <h1 class="text-2xl">Register</h1>
+  <div class="container mx-auto form-container">
+    <h1 class="mb-5 title-form">Cadastro</h1>
     <form @submit.prevent="register">
-      <input v-model="username" placeholder="Username" />
-      <input v-model="password" type="password" placeholder="Password" />
-      <button type="submit">Register</button>
+      <input v-model="username" placeholder="Username" 
+      class="input-form my-5"
+      required />
+      <input v-model="password" type="password" placeholder="Password" 
+      class="input-form my-2"
+      required/>
+      <button type="submit"
+      class="w-full min-h-[2.5em] min-w-[100px] text-dark font-bold text-lg rounded-lg cursor-pointer mt-5">
+      Cadastrar</button>
     </form>
+    <p class="mt-5 pt-5 text-sm">
+      Já possui conta?
+      <a href="/login" class="font-bold">Faça Login</a>
+    </p>
   </div>
 </template>
 
@@ -13,12 +23,14 @@
 import { ref } from "vue";
 import axios from "axios";
 import { useRouter } from "vue-router";
+import { useToast } from 'vue-toast-notification'
 
 export default {
   setup() {
     const username = ref("");
     const password = ref("");
     const router = useRouter();
+    const $toast = useToast();
 
     const apiUrl = import.meta.env.VITE_API_URL;
 
@@ -29,8 +41,10 @@ export default {
           { username: username.value, password: password.value }
         );
         router.push("/login");
-      } catch (error) {
+      } catch (error: any) {
         console.error("Erro no registro:", error);
+        const msg = error.response?.data?.message || 'Erro no login. Tente novamente.'
+        $toast.error(msg)
       }
     };
 
